2751bfc8f9ebdd471514c8ac6c6d9b00b9561696
amachkel <amachkel@gmail.com>
2022-03-10 13:34:34 -0500

    first 2 employee tests pass

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -2,28 +2,18 @@ const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
   //positive test
-  it("should create an object with 'name' string, 'ID' number, 'email' string, and 'title' string", () => {
-    const employee = new Employee("Fred", 12, "fred@gmail.com");
+  test("should create an object with 'name' string, 'ID' number, 'email' string, and 'title' string", () => {
+    const employee = new Employee("Fred", 12, "fred@gmail.com", "Employee");
     expect(employee.name).toEqual("Fred");
     expect(employee.id).toEqual(12);
     expect(employee.email).toEqual("fred@gmail.com");
     expect(employee.title).toEqual("Employee");
   });
-  //negative test
-  it("should throw an error if provided no arguments", () => {
-    // Wrap the object initialization in a callback function that Jest will run
-    const cb = () => new Employee();
 
-    // Verify that an error was thrown in the callback function
-    expect(cb).toThrow();
-  });
-  it("should throw an error if name is not a string", () => {
-    const cb = () => new Employee(3, 12, "fred@gmail.com", "Employee");
-
-    // Define the error message that is expected to be thrown
-    const err = new Error("Expected name to be a non-empty string");
-
-    // Verify that the correct error was thrown when the callback is executed
-    expect(cb).toThrowError(err);
+  test("should throw an error if name is not a string", () => {
+    const err = new Error("Expected parameter 'name' to be a non-empty string");
+    expect(() => {
+      new Employee(3, 12, "fred@gmail.com", "Employee").getName();
+    }).toThrow(err);
   });
 });
