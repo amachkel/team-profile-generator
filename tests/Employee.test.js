cf56b4a7e6d57b37c9e28beb8b9b4771ef9a8290
amachkel <amachkel@gmail.com>
2022-03-10 13:41:00 -0500

    Finished Employee tests. All pass.

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,9 +1,12 @@
 const Employee = require("../lib/Employee");
-
+// What data am I trying to get from the back end to create html for the browser?
+// pseudo code it out.
 describe("Employee", () => {
   //positive test
   test("should create an object with 'name' string, 'ID' number, 'email' string, and 'title' string", () => {
+    // Act
     const employee = new Employee("Fred", 12, "fred@gmail.com", "Employee");
+    // Assert
     expect(employee.name).toEqual("Fred");
     expect(employee.id).toEqual(12);
     expect(employee.email).toEqual("fred@gmail.com");
@@ -12,8 +15,37 @@ describe("Employee", () => {
 
   test("should throw an error if name is not a string", () => {
     const err = new Error("Expected parameter 'name' to be a non-empty string");
+    // Assert
     expect(() => {
       new Employee(3, 12, "fred@gmail.com", "Employee").getName();
+    }).toThrow(err);
+  });
+
+  //exception test
+  test("should throw an error if ID is not a number", () => {
+    const err = new Error(
+      "Expected parameter 'ID' to be a non-negative number"
+    );
+    expect(() => {
+      new Employee("Fred", "12", "fred@gmail.com", "Employee").getID();
+    }).toThrow(err);
+  });
+  //exception test
+  test("should throw an error if ID is less than 0", () => {
+    const err = new Error(
+      "Expected parameter 'ID' to be a non-negative number"
+    );
+    expect(() => {
+      new Employee("Fred", -12, "fred@gmail.com", "Employee").getID();
+    }).toThrow(err);
+  });
+  //exception test
+  test("should throw an error if email is not a string", () => {
+    const err = new Error(
+      "Expected parameter 'email' to be a non-empty string"
+    );
+    expect(() => {
+      new Employee("Fred", 12, 14, "Employee").getEmail();
     }).toThrow(err);
   });
 });
