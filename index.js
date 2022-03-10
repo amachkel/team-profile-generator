05c95ffb549c7bc09f29adf95cd8cf6a878ac74b
amachkel <amachkel@gmail.com>
2022-03-10 17:15:36 -0500

    Added employeeArr to push user data to.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const employee = require("./lib/Employee");
-const engineer = require("./lib/Engineer");
-const intern = require("./lib/Intern");
-const manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
 const questions = [
   {
@@ -29,9 +28,10 @@ const questions = [
     message: "Enter employee's email address.",
   },
 ];
-
+let employeeArr = [];
 function init() {
   inquirer.prompt(questions).then((data) => {
+    console.log(data.title);
     getEmployee(data);
   });
   //based on job title, start new inquirer function
@@ -47,7 +47,16 @@ getOfficeNum = (data) => {
     ])
     .then((response) => {
       data.officeNumber = response.officeNumber;
+      const employee = new Manager(
+        data.name,
+        data.id,
+        data.email,
+        data.title,
+        data.officeNumber
+      );
+      employeeArr.push(employee);
       console.log(data);
+      addAnother();
       console.log(
         `A new employee has been added: Title: ${data.title}, name: ${data.name}, id: ${data.id}, email: ${data.email}, office number: ${data.officeNumber}`
       );
@@ -64,9 +73,16 @@ getGitHub = (data) => {
     ])
     .then((response) => {
       data.github = response.github;
-      console.log(
-        `A new employee has been added: Title: ${data.title}, name: ${data.name}, id: ${data.id}, email: ${data.email}, office number: ${data.officeNumber}`
+      const employee = new Engineer(
+        data.name,
+        data.id,
+        data.email,
+        data.title,
+        data.github
       );
+      employeeArr.push(employee);
+      addAnother();
+      console.log(`A new employee has been added: ${employeeArr}`);
     });
 };
 getSchool = (data) => {
@@ -80,13 +96,21 @@ getSchool = (data) => {
     ])
     .then((response) => {
       data.school = response.school;
+      const employee = new Intern(
+        data.name,
+        data.id,
+        data.email,
+        data.title,
+        data.school
+      );
+      employeeArr.push(employee);
+      addAnother();
       console.log(
-        `A new employee has been added: Title: ${data.title}, name: ${data.name}, id: ${data.id}, email: ${data.email}, office number: ${data.officeNumber}`
+        `A new employee has been added: Title: ${data.title}, name: ${data.name}, id: ${data.id}, email: ${data.email}, school: ${data.school}`
       );
     });
 };
 const getEmployee = (data) => {
-  console.log(data.title);
   data.title == "Manager"
     ? getOfficeNum(data)
     : data.title == "Engineer"
@@ -94,5 +118,21 @@ const getEmployee = (data) => {
     : data.title == "Intern"
     ? getSchool(data)
     : console.error("error");
+};
+const addAnother = () => {
+  inquirer.prompt([
+    {
+      type: "checkbox",
+      name: "addAnother",
+      message: "What do you want to do now?",
+      choices: ["Add another employee", "I am done"],
+    },
+  ]).then((data) => {
+    if(data.addAnother != "I am done") {
+      init();
+    } else {
+      console.log(employeeArr);
+    }
+  });
 };
 init();
