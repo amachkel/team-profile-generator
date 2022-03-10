d37b8a35bc61954f7184273751ac28e1a7f073da
amachkel <amachkel@gmail.com>
2022-03-10 13:17:45 -0500

    created inquirer functions for each job title

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const manager = require("./lib/Manager");
 
 const questions = [
   {
+    type: "checkbox",
+    name: "title",
+    message: "What's the employee's job title?",
+    choices: ["Manager", "Engineer", "Intern"],
+  },
+  {
     type: "input",
     name: "name",
     message: "Enter employee's name.",
@@ -24,20 +30,46 @@ const questions = [
   },
 ];
 
-const managerQ = {
-  type: "input",
-  name: "officeNumber",
-  message: "Manager's office number: ",
-};
-
-const engineerQ = {
-  type: "input",
-  name: "github",
-  message: "GitHub username: ",
-};
-
-const internQ = {
-  type: "input",
-  name: "school",
-  message: "School name: ",
-};
+function init() {
+    inquirer.prompt(questions).then((data) => {
+      console.log(data.title);
+      data.title == "Manager"
+        ? getOfficeNum(data)
+        : data.title == "Engineer"
+        ? getGitHub(data)
+        : data.title == "Intern"
+        ? getSchool(data)
+        : console.error("error");
+    });
+    //then process and check employee type
+    //validate, based on validation, start new inquirer function
+  }
+  getOfficeNum = (data) => {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "Manager's office number: ",
+        },
+      ])
+      .then((response) => {
+        data.officeNumber = response.officeNumber;
+        console.log(data);
+      });
+  };
+  getGitHub = (data) => {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "github",
+          message: "GitHub username: ",
+        },
+      ])
+      .then((response) => {
+        data.github = response.github;
+        console.log(data);
+      });
+  };
+init();
